962d2b0a21410c2c8c769bfbfbb74b169aee4dea
liuxuhui <lxh_smh5558@163.com>
2018-10-22 18:21:33 +0800

    约束布局

--- app/src/main/java/com/botpy/constrainlayoutexample/MainActivity.java
+++ app/src/main/java/com/botpy/constrainlayoutexample/MainActivity.java
@@ -1,20 +1,36 @@
 package com.botpy.constrainlayoutexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(this);
+        ButterKnife.bind(this);
     }
 
-    @Override
+    @OnClick({R.id.button, R.id.button2})
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, ManualConstraintActivity.class);
+                startActivity(intent);
+                break;
 
+            case R.id.button2:
+                Toast.makeText(this, "点击注册", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/botpy/constrainlayoutexample/ManualConstraintActivity.java
@@ -0,0 +1,29 @@
+package com.botpy.constrainlayoutexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ManualConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_constraint);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
