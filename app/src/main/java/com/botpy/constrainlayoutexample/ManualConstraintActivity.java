f75ed27400c53e31ed14a4aff87d7dc2144a94bf
liuxuhui <lxh_smh5558@163.com>
2018-10-28 17:50:36 +0800

    添加输入框规则匹配

--- app/src/main/java/com/botpy/constrainlayoutexample/ManualConstraintActivity.java
+++ app/src/main/java/com/botpy/constrainlayoutexample/ManualConstraintActivity.java
@@ -5,14 +5,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 
+import com.botpy.constrainlayoutexample.view.ChineseEdittext;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ManualConstraintActivity extends AppCompatActivity {
+
+    @BindView(R.id.content_edit)
+    ChineseEdittext editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_constraint);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +34,8 @@ public class ManualConstraintActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editText.setInputType("[0-9\\.]+");
     }
 
 }
---
+++ app/src/main/java/com/botpy/constrainlayoutexample/view/ChineseEdittext.java
@@ -0,0 +1,191 @@
+package com.botpy.constrainlayoutexample.view;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputConnectionWrapper;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * Created by lzc on 2016/6/23.
+ */
+public class ChineseEdittext extends AppCompatEditText implements View.OnFocusChangeListener, TextWatcher {
+    /**
+     * 删除按钮的引用
+     */
+    private Drawable mClearDrawable;
+    private Context context;
+    private String regex;
+    /**
+     * 控件是否有焦点
+     */
+    private boolean hasFoucs;
+
+    public ChineseEdittext(Context context) {
+        this(context, null);
+        this.context = context;
+    }
+
+    public ChineseEdittext(Context context, AttributeSet attrs) {
+        // 这里构造方法也很重要，不加这个很多属性不能再XML里面定义
+        this(context, attrs, android.R.attr.editTextStyle);
+        this.context = context;
+    }
+
+    public ChineseEdittext(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+        init();
+    }
+
+    /**
+     * 输入法
+     * @param outAttrs
+     * @return
+     */
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        return new mInputConnecttion(super.onCreateInputConnection(outAttrs),
+                false);
+    }
+
+
+    private void init() {
+        // 获取EditText的DrawableRight,假如没有设置我们就使用默认的图片
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            // throw new
+            // NullPointerException("You can add drawableRight attribute in XML");
+//            mClearDrawable = getResources().getDrawable(R.drawable.chacha);
+        }
+
+//        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+        // 默认设置隐藏图标
+        setClearIconVisible(false);
+        // 设置焦点改变的监听
+        setOnFocusChangeListener(this);
+        // 设置输入框里面内容发生改变的监听
+        addTextChangedListener(this);
+    }
+
+    /**
+     * 因为我们不能直接给EditText设置点击事件，所以我们用记住我们按下的位置来模拟点击事件 当我们按下的位置 在 EditText的宽度 -
+     * 图标到控件右边的间距 - 图标的宽度 和 EditText的宽度 - 图标到控件右边的间距之间我们就算点击了图标，竖直方向就没有考虑
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (getCompoundDrawables()[2] != null) {
+
+                boolean touchable = event.getX() > (getWidth() - getTotalPaddingRight()) && (event.getX() < ((getWidth() - getPaddingRight())));
+
+                if (touchable) {
+                    this.setText("");
+                }
+            }
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 当ClearEditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
+     */
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        this.hasFoucs = hasFocus;
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+    }
+
+    /**
+     * 设置清除图标的显示与隐藏，调用setCompoundDrawables为EditText绘制上去
+     *
+     * @param visible
+     */
+    protected void setClearIconVisible(boolean visible) {
+        Drawable right = visible ? mClearDrawable : null;
+        setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
+    }
+
+    /**
+     * 设置匹配的类型
+     * @param regex
+     */
+    public void setInputType(String regex) {
+        this.regex = regex;
+    }
+
+    /**
+     * 当输入框里面内容发生变化的时候回调的方法
+     */
+    @Override
+    public void onTextChanged(CharSequence s, int start, int count, int after) {
+        if (hasFoucs) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    class mInputConnecttion extends InputConnectionWrapper implements
+            InputConnection {
+
+        public mInputConnecttion(InputConnection target, boolean mutable) {
+            super(target, mutable);
+        }
+
+        /**
+         * 对输入的内容进行拦截
+         *
+         * @param text
+         * @param newCursorPosition
+         * @return
+         */
+        @Override
+        public boolean commitText(CharSequence text, int newCursorPosition) {
+            if(TextUtils.isEmpty(regex)) {
+                // 默认只能输入汉字
+                regex = "[\u4e00-\u9fa5]+";
+            }
+            if (!text.toString().matches(regex)) {
+                Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            return super.commitText(text, newCursorPosition);
+        }
+
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            return super.sendKeyEvent(event);
+        }
+
+        @Override
+        public boolean setSelection(int start, int end) {
+            return super.setSelection(start, end);
+        }
+    }
+
+}
